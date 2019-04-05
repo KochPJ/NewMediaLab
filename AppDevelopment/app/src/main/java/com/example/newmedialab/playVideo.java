6ffee38f818dcf7e20f433197dd5d238948fd5d2
s1015256 <paul.koch@student.ru.nl>
2019-04-05 15:57:06 +0200

    last changes

--- AppDevelopment/app/src/main/java/com/example/newmedialab/playVideo.java
+++ AppDevelopment/app/src/main/java/com/example/newmedialab/playVideo.java
@@ -4,26 +4,67 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.TargetApi;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.VideoView;
 
 import org.jcodec.api.JCodecException;
+import org.jcodec.api.SequenceEncoder;
+import org.jcodec.common.DictionaryCompressor;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class playVideo extends AppCompatActivity {
     VideoView videov;
     Video v;
+    ImageView image;
+    Handler handler=new Handler();
+    Bitmap b;
+
+
 
     @TargetApi(Build.VERSION_CODES.P)
     @RequiresApi(api = Build.VERSION_CODES.N)
     public void playVideo(View view) throws InterruptedException, IOException, JCodecException {
 
+        image = (ImageView) findViewById(R.id.imview);
+
+
+        MediaMetadataRetriever m = new MediaMetadataRetriever();
+        String videopath ="android.resource://" + getPackageName()+ "/" + R.raw.test;
+        m.setDataSource(this, Uri.parse(videopath));
+
+        int max = (int) m.METADATA_KEY_DURATION;
+        int fps = m.METADATA_KEY_CAPTURE_FRAMERATE;
+        int timestep = (int)(1000.0*(float)max/(float)fps);
+
+        Bitmap b;
+        List<Bitmap> video = new ArrayList<Bitmap>();
+        for (int i=0;i<max*fps;i++){
+
+            Log.i("i",String.valueOf(i));
+            b = m.getFrameAtTime(i*timestep,MediaMetadataRetriever.OPTION_CLOSEST);
+            video.add(b);
+            image.setImageBitmap(b);
+        }
+
+
+
+/*
 
         InputStream ins = getResources().openRawResource(getResources().getIdentifier("test", "raw", getPackageName()));
         String path = getFilesDir().getAbsolutePath();
@@ -36,7 +77,8 @@ public class playVideo extends AppCompatActivity {
             Log.d("Files", "FileName:" + files[i].getName());
         }
 
-        v = new Video(ins);
+       // v = new Video(ins);
+
 
         Log.d("Files", "Path: " + path);
         directory = new File(path);
@@ -46,6 +88,7 @@ public class playVideo extends AppCompatActivity {
         {
             Log.d("Files", "FileName:" + files[i].getName());
         }
+*/
 
 
         }
