aea503730469da7c8776a1d5ad22fab0d0732522
s1015256 <paul.koch@student.ru.nl>
2019-04-10 19:38:00 +0200

    added caputre video and load video function to add stimuli activity

--- AppDevelopment/app/src/main/java/com/example/newmedialab/AddStimuli.java
+++ AppDevelopment/app/src/main/java/com/example/newmedialab/AddStimuli.java
@@ -1,15 +1,64 @@
 package com.example.newmedialab;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddStimuli extends AppCompatActivity {
 
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+    private static final int SELECT_VIDEO = 2;
+    VideoView videoView;
+
+    public void captureVideo(View view) {
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        }
+    }
+
+    public void loadStimuli(View view) {
+        Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, SELECT_VIDEO);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_VIDEO) {
+                Uri videoPath = data.getData();
+                String Path = videoPath.getPath();
+                Log.d("addStimuli", "Path = "+Path);
+                videoView.setVideoURI(videoPath);
+                videoView.start();
+            }
+        }
+    }
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_stimuli);
+        videoView = (VideoView)findViewById(R.id.videoViewNewStimuli);
     }
+
+
 
 }
