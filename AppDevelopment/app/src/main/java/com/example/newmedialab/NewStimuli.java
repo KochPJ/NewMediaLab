3b4b88b88868ab0eb67ff15c0117b5f891849c1e
mjjdg <mdegrauwmobiel@gmail.com>
2019-04-16 13:18:26 +0200

    Version 4 of 16th of April

--- AppDevelopment/app/src/main/java/com/example/newmedialab/NewStimuli.java
+++ AppDevelopment/app/src/main/java/com/example/newmedialab/NewStimuli.java
@@ -3,7 +3,6 @@ package com.example.newmedialab;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -21,7 +20,6 @@ import java.util.ArrayList;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewStimuli extends AppCompatActivity {
-
     static final int REQUEST_VIDEO_CAPTURE = 1;
     static final int SELECT_VIDEO = 2;
     ArrayList<Uri> userSelectedVideoUriList;
@@ -31,15 +29,12 @@ public class NewStimuli extends AppCompatActivity {
     String video_name = "video_1";
     VideoView videoView;
     Integer lastAdd = -1;
-    Uri currentVideo = Uri.parse(Environment.getExternalStorageDirectory()+"/KineTest/CurrentVideo/loadedVideo.mp4");
-    Video videoloaded = new Video(this, "loadedVideo", 25,4,currentVideo);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_stimuli);
         videoView = (VideoView)findViewById(R.id.vv_new_stimuli);
-
         if (!OpenCVLoader.initDebug()) {
             Log.d("OpenCV", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
             OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
@@ -47,48 +42,41 @@ public class NewStimuli extends AppCompatActivity {
             Log.d("OpenCV", "OpenCV library found inside package. Using it!");
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }
-
     }
-
     public void FinishAdding(View view) {
         //TODO: set correct in/out dirs, add name
         // Get video name
         EditText et_video_name = (EditText) findViewById(R.id.et_video_name);
         this.video_name = et_video_name.getText().toString();
-
         if (lastAdd != -1) {
             //TODO: set correct intent
-            Intent intent = new Intent(this, MainActivity.class);
+            Intent intent = new Intent(this, AddStimuli.class);
+            intent = intent.putExtra("video_name", this.video_name);
+            intent = intent.putExtra("video_uri", this.userSelectedVideoUriList.get(this.userSelectedVideoUriList.size() - 1));
             startActivity(intent);
         }else{
             Log.d("NewStimuli", "playStimuli: no video");
             Toast.makeText(this, "video not loaded yet", Toast.LENGTH_SHORT).show();
         }
     }
-
     public void filmVideo(View view) {
         Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
         startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
     }
-
     /* Invoke android os system file browser to select images. */
     public void loadVideo(View view)
     {
         // Create an intent.
         Intent openVideoIntent = new Intent();
-
         // Only show images in the content chooser.
         // If you want to select all type data then openAlbumIntent.setType("*/*");
         // Must set type for the intent, otherwise there will throw android.content.ActivityNotFoundException: No Activity found to handle Intent { act=android.intent.action.GET_CONTENT }
         openVideoIntent.setType("video/mp4");
-
         // Set action, this action will invoke android os browse content app.
         openVideoIntent.setAction(Intent.ACTION_GET_CONTENT);
-
         // Start the activity.
         startActivityForResult(openVideoIntent, SELECT_VIDEO);
     }
-
     /* When the action Intent.ACTION_GET_CONTENT invoked app return, this method will be executed. */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -98,7 +86,6 @@ public class NewStimuli extends AppCompatActivity {
             {
                 if(userSelectedVideoUriList == null) {userSelectedVideoUriList = new ArrayList<Uri>(); }
                 //if(userSelectedVideoDirectoryList == null) {userSelectedVideoDirectoryList = new ArrayList<String>(); }
-
                 Uri fileUri = data.getData();
                 userSelectedVideoUriList.add(fileUri);
                 Log.d("NewStimuli: ", "video uri: "+fileUri.toString());
@@ -108,7 +95,6 @@ public class NewStimuli extends AppCompatActivity {
             }
         }
     }
-
     public void playVideo(View view){
         if(this.lastAdd == 1){
             Uri outputVideo = this.userSelectedVideoUriList.get(this.userSelectedVideoUriList.size() - 1);
@@ -119,7 +105,6 @@ public class NewStimuli extends AppCompatActivity {
             Toast.makeText(this, "video not loaded yet", Toast.LENGTH_SHORT).show();
         }
     }
-
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
