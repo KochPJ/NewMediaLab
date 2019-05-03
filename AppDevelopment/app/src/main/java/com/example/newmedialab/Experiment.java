0581ea966095f0ed50a8ed6a19c251573201e705
s1015256 <paul.koch@student.ru.nl>
2019-05-03 18:10:52 +0200

    exp

--- AppDevelopment/app/src/main/java/com/example/newmedialab/Experiment.java
+++ AppDevelopment/app/src/main/java/com/example/newmedialab/Experiment.java
@@ -47,11 +47,10 @@ public class Experiment implements Serializable{
     }
 
     public void deleteExperiment(){
-        Log.d("exp", "path = "+Environment.getExternalStorageDirectory()+"/KineTest/Experiments/"+name);
+        //delete folder
         File dir = new File(Environment.getExternalStorageDirectory()+"/KineTest/Experiments/"+name);
         if (dir.isDirectory())
         {
-            Log.d("exp", "deleting exp");
             try {
                 FileUtils.deleteDirectory(dir);
             } catch (IOException e) {
@@ -59,20 +58,14 @@ public class Experiment implements Serializable{
             }
         }
 
-        dir = new File(Environment.getExternalStorageDirectory()+"/KineTest/Experiments/Experiment_files/");
-        Log.d("exp", "path ="+Environment.getExternalStorageDirectory()+"/KineTest/Experiments/Experiment_files");
+        //delete file
+        dir = new File(Environment.getExternalStorageDirectory()+"/KineTest/Experiments/Experiment_files");
         if (dir.isDirectory())
         {
-
-            Log.d("exp", "deleting exp file");
-            dir.delete();
             String[] children = dir.list();
             for (int i = 0; i < children.length; i++)
             {
-
-                Log.d("exp", "checking file name = "+children[i]);
                 if(children[i].equals(name+".txt")){
-                    Log.d("exp", "deleting exp file = "+children[i]);
                     new File(dir, children[i]).delete();
                 }
             }
